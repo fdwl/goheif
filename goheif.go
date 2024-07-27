217e621078e3f676d78d1167a68ba4ffde302ccc
noah-mac <fdwl@msn.com>
2024-07-27 18:14:36 +0800

    ldd

--- goheif.go
+++ goheif.go
@@ -8,8 +8,8 @@ import (
 	"io"
 	"io/ioutil"
 
-	"github.com/jdeng/goheif/heif"
-	"github.com/jdeng/goheif/libde265"
+	"github.com/fdwl/goheif/heif"
+	"github.com/fdwl/goheif/libde265"
 )
 
 // SafeEncoding uses more memory but seems to make
